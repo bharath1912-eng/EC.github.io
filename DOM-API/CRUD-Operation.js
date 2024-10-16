9fb3f0f691acfe02638264621e9adbc9b0d819aa
bharath <bharathparthi1912@gmail.com>
2024-10-16 07:58:01 -0700

    add

--- DOM-API/CRUD-Operation.js
+++ DOM-API/CRUD-Operation.js
@@ -17,4 +17,42 @@ createTag.className='javaScript'
 createTag.title='javaScript Traning'
 //delete
 const elements = document.getElementsByClassName('selectClass')[0];
-elements.remove();
+//elements.remove();
+  
+// Creating a element and set Attribute the Existing Element
+// and Create a parent Element a move the existing as child element
+const createElement=document.createElement('div');
+createElement.setAttribute('id','setAttribute');
+createElement.setAttribute('title','Java Script traning');
+createElement.appendChild(elements);
+selectBasedOnId.prepend(createElement);
+
+//getting attribute
+const getAttribute=createElement.getAttribute('id');
+// removing Attribute
+
+createElement.removeAttribute('title');
+
+// checking if the Attribute is present or not
+createElement.hasAttribute('title');// it return a boolean value
+
+//adding a new class to a existing class
+elements.classList.add('addClass');
+
+//events 
+// add Event Listener and Removeing the Event Listener after executed once
+
+function showMessage() {
+    alert('Event listener going remove')
+     createTag.removeEventListener("click", showMessage);
+ }
+
+ // Add event listener to execute once
+ createTag.addEventListener("click", showMessage);
+
+ //prevent default behaviour
+ const btn=document.getElementById('button');
+ btn.addEventListener('click',(event) =>{
+    event.preventDefault();
+
+ })
